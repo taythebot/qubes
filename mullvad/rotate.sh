917f3474406e6be7fe34c5137e384d3a415ffc56
Tay <60461479+taythebot@users.noreply.github.com>
2023-03-13 21:19:19 +0000

    Fix mullvad rotation script

--- mullvad/rotate.sh
+++ mullvad/rotate.sh
@@ -6,9 +6,6 @@ ACCOUNT_FILE="/home/user/mullvad.txt"
 # Wireguard configuration file
 CONFIG="/rw/config/wireguard/wg0.conf"
 
-# Mullvad certificate
-MULLVAD_CERT="certificate.pem"
-
 # Escape special characters for sed
 escape() {
 	echo $1 | sed -e 's/[\/&]/\\&/g'
@@ -56,31 +53,18 @@ echo "[+] Generated new public key $NEW_PUBLIC_KEY"
 
 # Get authorization token
 echo "[+] Getting Mullvad auth token"
-ACCOUNT_RES=$(curl -s --cacert $MULLVAD_CERT https://api.mullvad.net/www/accounts/$ACCOUNT/)
+ACCOUNT_RES=$(curl -s https://api.mullvad.net/www/accounts/$ACCOUNT/)
 AUTH_TOKEN=$(echo $ACCOUNT_RES | jq -r '.auth_token')
 if [ -z $AUTH_TOKEN ]; then
 	echo "[+] Error: Failed to get account token"
 	exit 1
 fi
 
-# Check if existing key is in Mullvad account
-ACCOUNT_KEYS=$(echo $ACCOUNT_RES | jq -r '.account.wg_peers[].key.public')
-if grep -q $OLD_PUBLIC_KEY <<< $ACCOUNT_KEYS; then
-	echo "[+] Removing old key from Mullvad"
-
-	curl -s --cacert $MULLVAD_CERT \
-		-H "Content-Type: application/json" \
-		-H "Authorization: Token $AUTH_TOKEN" \
-		-d '{"pubkey": "'$OLD_PUBLIC_KEY'"}' \
-		https://api.mullvad.net/www/wg-pubkeys/revoke/
-fi
-
 # Submit new key to Mullvad
 echo "[+] Submitting new Wireguard key to Mullvad"
-IPS=$(curl -s --cacert $MULLVAD_CERT \
-	-d account="$ACCOUNT" \
-	--data-urlencode pubkey="$NEW_PUBLIC_KEY" \
-	https://api.mullvad.net/wg/)
+IPS=$(curl -s -d account="$ACCOUNT" \
+	      --data-urlencode pubkey="$NEW_PUBLIC_KEY" \
+	      https://api.mullvad.net/wg/)
 if ! printf '%s\n' "$IPS" | grep -E '^[0-9a-f:/.,]+$' >/dev/null
 then
         echo "[+] Error: Failed to submit new Wireguard key to Mullvad"
@@ -106,3 +90,14 @@ sleep 60
 # Restart Wireguard
 echo "[+] Restarting Wireguard"
 systemctl restart wg-quick@wg0
+
+# Check if existing key is in Mullvad account
+ACCOUNT_KEYS=$(echo $ACCOUNT_RES | jq -r '.account.wg_peers[].key.public')
+if grep -q $OLD_PUBLIC_KEY <<< $ACCOUNT_KEYS; then
+        echo "[+] Removing old key from Mullvad"
+
+        curl -s -H "Content-Type: application/json" \
+                -H "Authorization: Token $AUTH_TOKEN" \
+                -d '{"pubkey": "'$OLD_PUBLIC_KEY'"}' \
+                https://api.mullvad.net/www/wg-pubkeys/revoke/
+fi
